512c1e99e886ef1283d443cc2547521ce9f4ca59
osmani.perez <osmanisperez4@gmail.com>
2025-01-18 21:00:16 -0600

    added glow on tiles for todays date

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).on("click", ".tile", clickMouse);
 $(document).on("click", "#reset-button", resetTiles);
+$(window).on("load", highlightTodaysDate);
 
 let pos1 = 0,
   pos2 = 0,
@@ -175,4 +176,15 @@ function resetTiles() {
     tile.style.removeProperty("top");
     tile.style.removeProperty("left");
   }
+}
+
+function highlightTodaysDate() {
+  const dateObj = new Date();
+  const date = dateObj.getDate();
+  const month = dateObj.getMonth();
+  const day = dateObj.getDay();
+
+  document.getElementById(`tile-space-date-${date}`).classList.add("glow");
+  document.getElementById(`tile-space-month-${month}`).classList.add("glow");
+  document.getElementById(`tile-space-day-${day}`).classList.add("glow");
 }
